bdb3e9d8c88acd9d70cd41ed2bf33f3db3c6dd19
Thomas <tllobregat@coreoz.com>
2022-03-30 01:12:27 +0200

    production

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from app.main import app
 
 if __name__ == "__main__":
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=8080)
-    app.run()
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    # app.run()
