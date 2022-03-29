aad442bf6243318d5af22855c95503bca197eb61
Thomas <tllobregat@coreoz.com>
2022-03-30 01:09:33 +0200

    test

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ import requests
 app = Flask(__name__)
 
 class Utils:
-    # model_url = "https://embeddings.net/embeddings/frWac_non_lem_no_postag_no_phrase_200_skip_cut100.bin"
-    model_url = "data/frWac_non_lem_no_postag_no_phrase_200_cbow_cut100.bin"
+    model_url = "https://embeddings.net/embeddings/frWac_non_lem_no_postag_no_phrase_200_cbow_cut100.bin"
+    # model_url = "data/frWac_non_lem_no_postag_no_phrase_200_cbow_cut100.bin"
     loading = False
     today_s_word = ''
     tried = 0
