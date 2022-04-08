5b7c028055ebeb163c0fdec58ef8d027bc552d8a
Thomas <tllobregat@coreoz.com>
2022-04-09 00:56:21 +0200

    add cors + front data WS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from utils import utils
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/init', methods=['GET'])
@@ -46,5 +48,29 @@ def history():
                 'word_history': row[5],
                 'guess_history': row[6],
             } for row in rows
+        ]
+    }
+
+
+@app.route('/front-data', methods=['GET'])
+def front_data():
+    row = utils.get_todays_row()
+    index_history = row[2][1:-1].split(", ")
+    word_history = row[5][1:-1].split(", ")[:-1]
+    guess_history = row[6][1:-1].split(", ")[:-1]
+
+    if len(index_history) != len(word_history) or len(word_history) != len(guess_history):
+        raise Exception("Internal Error")
+
+    return {
+        'today_s_date': row[0],
+        'today_s_word': row[1],
+        'time_to_found': row[3],
+        'word_history': [
+            {
+                "index": index_history[i],
+                "word": word_history[i].replace("'", ""),
+                "guess": guess_history[i]
+            } for i in range(len(index_history))
         ]
     }
